8fb913b7c239c7aeb142d5d16caf41a08b67f45e
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2026-03-04 23:08:05 -0300

    add error handling for division by zero and small numbers in calculate function

--- src/main.rs
+++ src/main.rs
@@ -23,4 +23,26 @@ fn calculate() {
 
     let divide_result: f64 = arithmetics::divide(a, b);
     println!("The division of {} and {} is {}", a, b, divide_result);
+
+    let zero: f64 = 0.0;
+    let mut near_zero: f64 = 0.01;
+    near_zero = near_zero.powf(10.0);
+    println!(
+        "Now let's try to divide by zero ({}) and a very small number: {}",
+        zero, near_zero
+    );
+    let div1 = std::panic::catch_unwind(|| {
+        arithmetics::divide(a, zero);
+    });
+    match div1 {
+        Ok(_) => println!("Division by zero succeeded"),
+        Err(_) => println!("Division by zero caused a panic"),
+    }
+    let div2 = std::panic::catch_unwind(|| {
+        arithmetics::divide(a, near_zero);
+    });
+    match div2 {
+        Ok(_) => println!("Division by a very small number succeeded"),
+        Err(_) => println!("Division by a very small number caused a panic"),
+    }
 }
